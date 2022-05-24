b9bf2ae9feeb81f61e01562b4c1a8ae748ad6301
Gibran Castillo <gibran-castillo@hotmail.com>
2022-05-23 22:16:18 -0500

    Add search functionality and retrieve OMDB info

---
+++ src/js/omdb_api.js
@@ -0,0 +1,21 @@
+document.querySelector("#search-btn").addEventListener("click", () => {
+  const omdbSearch = document.querySelector("#search").value;
+  const omdbRequestUrl = "https://www.omdbapi.com/?apikey=7f7fde0a&s=" + omdbSearch;
+  
+  fetch(omdbRequestUrl)
+    .then(response => {
+      const contentType = response.headers.get("content-type");
+      if (!contentType || !contentType.includes("application/json")) {
+        throw new TypeError("Oops, we haven't got JSON!");
+      }
+      return response.json();
+    })
+    .then(jsonObject => {
+      /* process your data further */
+      console.table(jsonObject); // temporary checking for valid response and data parsing
+      
+    })
+    .catch(error => console.error(error));
+});
+
+//const omdbDetailRequestUrl = "https://www.omdbapi.com/?apikey=7f7fde0a&t=" + title;
