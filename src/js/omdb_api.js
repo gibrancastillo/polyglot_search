c8d4d273c9d53b455cc5744c37d5a437657c0d98
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2022-05-28 15:04:30 -0600

    Movie Ditails

--- src/js/omdb_api.js
+++ src/js/omdb_api.js
@@ -2,7 +2,7 @@ document.querySelector("#translate-search-btn").addEventListener("click", () => 
   let omdbRequestUrl = "";
   const omdbSearch = document.querySelector("#search-phrase").value;
   
-  if(window.location.pathname == "/") {
+  if(window.location.pathname == "/index.html") {
     omdbRequestUrl = "https://www.omdbapi.com/?apikey=7f7fde0a&s=" + omdbSearch;
   } else {
     omdbRequestUrl = "https://www.omdbapi.com/?apikey=7f7fde0a&t=" + omdbSearch;
@@ -20,14 +20,17 @@ document.querySelector("#translate-search-btn").addEventListener("click", () => 
       /* process your data further */
       console.table(jsonObject); // temporary checking for valid response and data parsing
 
-      if(window.location.pathname == "/") {
+      if(window.location.pathname == "/index.html") {
+        createBanner();
         loadMovies(jsonObject);
+        
       } else {
-        //loadMovie(jsonObject);
+        createBanner();
+        loadMovie(jsonObject);
         console.log("loadMovie(jsonObject)");
       }
       
-      createBanner();
+      
     })
     .catch((error) => console.error(error));
 });
@@ -107,4 +110,62 @@ function textBaner(){
   <use xlink:href="#s-text" class="text"></use>
 
 </svg>`;
+}
+function loadMovie(jsonObject){
+  const movie = jsonObject["Search"];
+
+  let anchored = document.createElement("a");
+  anchored.href = "page1/index.html"
+  card.appendChild(anchored);
+
+  // Create card (section element)
+  let card = document.createElement("section");
+
+  // h2 Title
+  let h2 = document.createElement("h2");
+  h2.textContent = movie.Title;
+  card.appendChild(h2);
+    // Image
+  let image = document.createElement("img");
+
+  if (movie.Poster == "N/A") {
+    image.setAttribute("src", "../assets/images/marketing/NOimages.png");
+  } else {
+    image.setAttribute("src", movie.Poster);
+  }
+  image.setAttribute("alt", movie.Title);
+  card.appendChild(image);
+  // h4 Title
+  let h4 = document.createElement("h4");
+  h4.textContent = "Actors: " + movie.Actors;
+  card.appendChild(h4);
+  // h5 Title
+  let h5 = document.createElement("h5");
+  h5.textContent = "Writer: " + movie.Writer;
+  card.appendChild(h5);
+  //h6 Tittle
+  let h6 = document.createElement("h6");
+  h6.textContent = "Writer: " + movie.Writer;
+  card.appendChild(h6);
+  //Title
+  let h62 = document.createElement("h6");
+  h62.textContent = "Genre: " + movie.Genre;
+  card.appendChild(h62);
+  //Title
+  let p = document.createElement("p");
+  p.textContent = "Plot: " + movie.Plot;
+  card.appendChild(p);
+  //Title
+  let h63 = document.createElement("h6");
+  h63.textContent = "Language: " + movie.Language;
+  card.appendChild(h63);
+  //Title
+  let h64 = document.createElement("h6");
+  h64.textContent = "Date: " + movie.Released;
+  card.appendChild(h64);
+  // Add card (section element) to <a> (anchored) element
+  anchored.appendChild(card)
+
+  // Add anchored (<a> element) to class "cards" div element
+  document.querySelector("div.cards").appendChild(anchored);
 }
